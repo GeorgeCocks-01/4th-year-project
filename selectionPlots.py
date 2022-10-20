f3d75398ec4049beb83680f071733a4ffb1e28fc
George Cocks <georgecocks01@gmail.com>
2022-10-20 19:41:14 +0100

    Created selectionPlots.py, which is a copy of test_ZHtt.py but with the selection cut of 2 leptons in final state only. It outputs tau pT sum, di lepton mass, met pT sum, delta R and delta Eta for both Z and H decay.

--- selectionPlots.py
+++ selectionPlots.py
@@ -9,7 +9,7 @@ import argparse
 #  e.g. /my/directory/a*test*/pattern/*.root
 #  @param pattern    the file name pattern using python regular expressions
 #  @return list of all files matching the pattern
-def findAllFilesInPath( pattern, path ):
+def findAllFilesInPath( pattern ,path ):
   files = []
   items = pattern.split( '/' )
 
@@ -55,29 +55,67 @@ def main(args):
   print(nFiles, "files")
 
   # define histograms
-  pTt0 = ROOT.TH1D("tau0_pt", "p_{T}^{#tau_0}", 150, 0, 300)
-  pTt0.Sumw2()
-  pTt1 = pTt0.Clone("tau1_pt")
-  pTt1.SetTitle("p_{T}^{#tau_1}")
+  pTtsum = ROOT.TH1D("tau_pt_sum", "p_{T}^{#tau_sum}", 200, 0, 500)
+  diLeptonMass = ROOT.TH1D("lepton_mass_sum", "M(ll)", 150, 0, 200)
+  metpTsum = ROOT.TH1D("met_pt_sum", "met.Pt()_sum", 150, 0, 500)
+  deltaRZ = ROOT.TH1D("delta_R_Z", "delta_R_Z", 150, 0, 5)
+  deltaRH = ROOT.TH1D("delta_R_H", "delta_R_H", 150, 0, 5)
+  deltaEtaZ = ROOT.TH1D("delta_Eta_Z", "delta_Eta_Z", 150, 0, 5)
+  deltaEtaH = ROOT.TH1D("delta_Eta_H", "delta_Eta_H", 150, 0, 5)
+  pTtsum.Sumw2()
+  diLeptonMass.Sumw2()
+  metpTsum.Sumw2()
+  deltaRZ.Sumw2()
+  deltaRH.Sumw2()
+  deltaEtaZ.Sumw2()
+  deltaEtaH.Sumw2()
 
+  #FILL HISTOGRAMS LOOP
   for i in range(0, tree.GetEntries()):
     tree.GetEntry(i)
-    #vector of TLorentzVectors of the taus
+    # vector of TLorentzVectors of the taus
     taus_p4 = getattr(tree, "taus_p4")
+    leptons_p4 = getattr(tree, "leptons_p4")
+    met_p4 = getattr(tree, "met_p4")
+    lFlavour = getattr(tree, "leptons")
+    lCharge = getattr(tree, "leptons_q")
+
     # we need to have at least 2 taus
     if len(taus_p4) > 1:
-      pt = taus_p4[0].Pt()
-      pTt0.Fill(pt)
-      pt = taus_p4[1].Pt()
-      pTt1.Fill(pt)
-      #print(pt)
+      # selection cut for 2 leptons in final state
+      if (len(leptons_p4) == 2) and (lFlavour[0] == lFlavour[1]) and (lCharge[0] == -lCharge[1]):
+        pt = taus_p4[0].Pt() + taus_p4[1].Pt()
+        leptonMass = (leptons_p4[0] + leptons_p4[1]).M() # maybe faster to do (leptons_p4[0].M() + leptons_p4[1].M())?
+        dRZ = leptons_p4[0].DeltaR(leptons_p4[1])
+        dRH = taus_p4[0].DeltaR(taus_p4[1])
+        dEtaZ = leptons_p4[0].DeltaR(leptons_p4[1])
+        dEtaH = taus_p4[0].DeltaR(taus_p4[1])
 
+        pTtsum.Fill(pt)
+        diLeptonMass.Fill(leptonMass)
+        metpTsum.Fill(met_p4.Pt())
+        deltaRZ.Fill(dRZ)
+        deltaRH.Fill(dRH)
+        deltaEtaZ.Fill(dEtaZ)
+        deltaEtaH.Fill(dEtaH)
+
+      # selection cut for 3 leptons in final state
+      #elif (len(leptons_p4) == 3) and (lFlavour[0] )
+
+  #OUTPUT LOOP
   if (args.outputfile[-5:] != ".root"): #adds .root to end of output file if not present
     args.outputfile += ".root"
   outHistFile = ROOT.TFile.Open(args.outputfile, "RECREATE")
   outHistFile.cd()
-  pTt0.Write()
-  pTt1.Write()
+
+  pTtsum.Write()
+  diLeptonMass.Write()
+  metpTsum.Write()
+  deltaRZ.Write()
+  deltaRH.Write()
+  deltaEtaZ.Write()
+  deltaEtaH.Write()
+
   outHistFile.Close()
 
   del tree
