f00624bd6fe36aeffaf7faaed789bfe99e9315a5
George Cocks <georgecocks01@gmail.com>
2022-12-01 00:29:48 +0000

    Added in new datasets

--- runBackground.sh
+++ runBackground.sh
@@ -5,3 +5,6 @@ nohup python selectionPlots.py -i llll -o llll-weighted.root >> backgroundOutput
 nohup python selectionPlots.py -i lllv -o lllv-weighted.root >> backgroundOutput.txt &
 nohup python selectionPlots.py -i llvv -o llvv-weighted.root >> backgroundOutput.txt &
 nohup python selectionPlots.py -i ttH -o ttH-weighted.root >> backgroundOutput.txt &
+nohup python selectionPlots.py -i llll_lowMllPtComplement -o llll_lowMllPtComplement-weighted.root >> backgroundOutput.txt &
+nohup python selectionPlots.py -i lllv_lowMllPtComplement -o lllv_lowMllPtComplement-weighted.root >> backgroundOutput.txt &
+nohup python selectionPlots.py -i llvv_lowMllPtComplement -o llvv_lowMllPtComplement-weighted.root >> backgroundOutput.txt &
