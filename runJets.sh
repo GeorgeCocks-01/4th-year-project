e146b4cbf4788133f0abe780720882cd5f90db2f
George Cocks <georgecocks01@gmail.com>
2022-12-12 18:03:28 +0000

    Fixed filenames

--- runJets.sh
+++ runJets.sh
@@ -14,8 +14,8 @@ nohup python selectionPlots.py -i Zee_MV70_140_BF >> jetsOutput.txt &
 nohup python selectionPlots.py -i Zee_MV70_140_CFBV >> jetsOutput.txt &
 nohup python selectionPlots.py -i Zee_MV70_140_CVBV >> jetsOutput.txt &
 nohup python selectionPlots.py -i Zmumu_MV0_70_BF >> jetsOutput.txt &
-nohup python selectionPlots.py -i Zmumu_MV0_70_CFBV.D1 >> jetsOutput.txt &
-nohup python selectionPlots.py -i Zmumu_MV0_70_CVBV.D1 >> jetsOutput.txt &
+nohup python selectionPlots.py -i Zmumu_MV0_70_CFBV >> jetsOutput.txt &
+nohup python selectionPlots.py -i Zmumu_MV0_70_CVBV >> jetsOutput.txt &
 nohup python selectionPlots.py -i Zmumu_MV1000_E_CMS >> jetsOutput.txt &
 nohup python selectionPlots.py -i Zmumu_MV140_280_BF >> jetsOutput.txt &
 nohup python selectionPlots.py -i Zmumu_MV140_280_CFBV >> jetsOutput.txt &
