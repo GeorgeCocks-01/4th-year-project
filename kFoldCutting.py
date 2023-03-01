97ab6d406a2079660ccc03dc07583f3e20b59008
George <georgecocks01@gmail.com>
2023-03-01 16:06:32 +0000

    Added in code to print out yields and plot stacked histograms

--- kFoldCutting.py
+++ kFoldCutting.py
@@ -22,10 +22,10 @@ def main():
   for cut, threshold in zip(("2lep", "3lep"), thresholds):
     # dictionary of the delta phi ll arrays for each sample
     delta_phi_ll_dict = {
-      "signal": np.array([]),
       "llll": np.array([]),
       "other di-boson": np.array([]),
-      "jets": np.array([])
+      "jets": np.array([]),
+      "signal": np.array([])
     }
     weight_dict = dict.fromkeys(delta_phi_ll_dict, np.array([]))
     predictions_dict = dict.fromkeys(delta_phi_ll_dict, np.array([]))
@@ -50,25 +50,29 @@ def main():
       prediction = f["prediction"].array(library = "np")
       group_number = f["group"].array(library = "np")
 
+    # Dictionary of group numbers
+    group_dict = {
+      "llll": 1,
+      "other di-boson": 2,
+      "jets": 3,
+      "signal": 0
+    }
+
     # Sort predictions into groups
-    for i, key in enumerate(predictions_dict):
-      predictions_dict[key] = prediction[group_number == i]
+    for key in predictions_dict:
+      predictions_dict[key] = prediction[group_number == group_dict[key]]
 
     # Cut delta phi ll arrays using threshold
     for key in delta_phi_ll_dict:
-      # print(len(delta_phi_ll_dict[key]))
-      # print(len(predictions_dict[key]))
       delta_phi_ll_dict[key] = delta_phi_ll_dict[key][predictions_dict[key] > threshold]
       weight_dict[key] = weight_dict[key][predictions_dict[key] > threshold]
 
-    print("Number of events after cut for " + cut + ":", sum([len(delta_phi_ll_dict[key]) for key in delta_phi_ll_dict]))
-
     # Generate histogram for output
     delta_phi_ll_histograms =  {
-      "signal": ROOT.TH1D("signal" + cut, "delta_phi_ll" + cut + ";Delta Phi(Rad);Normalised Counts", 50, -4, 4),
-      "llll": ROOT.TH1D("llll" + cut, "delta_phi_ll" + cut + ";Delta Phi(Rad);Normalised Counts", 50, -4, 4),
-      "other di-boson": ROOT.TH1D("di-boson" + cut, "delta_phi_ll" + cut + ";Delta Phi(Rad);Normalised Counts", 50, -4, 4),
-      "jets": ROOT.TH1D("jets" + cut, "delta_phi_ll" + cut + ";Delta Phi(Rad);Normalised Counts", 50, -4, 4)
+      "signal": ROOT.TH1D("signal" + cut, "delta_phi_ll" + cut + ";Delta Phi(Rad);Normalised Counts", 4, -4, 4),
+      "llll": ROOT.TH1D("llll" + cut, "delta_phi_ll" + cut + ";Delta Phi(Rad);Normalised Counts", 4, -4, 4),
+      "other di-boson": ROOT.TH1D("di-boson" + cut, "delta_phi_ll" + cut + ";Delta Phi(Rad);Normalised Counts", 4, -4, 4),
+      "jets": ROOT.TH1D("jets" + cut, "delta_phi_ll" + cut + ";Delta Phi(Rad);Normalised Counts", 4, -4, 4)
     }
 
     # Fill histogram with delta phi ll values
@@ -76,6 +80,20 @@ def main():
       arr = array('d', delta_phi_ll_dict[key])
       weight_arr = array('d', weight_dict[key])
       delta_phi_ll_histograms[key].FillN(len(arr), arr, weight_arr)
+
+    # Print yields
+    print("Yields for " + cut + " cut")
+    background_yield = 0
+    for key in delta_phi_ll_histograms:
+      if key == "signal":
+        signal_yield = delta_phi_ll_histograms[key].Integral(0, 5)
+        print("Signal yield: ", signal_yield)
+      else:
+        temp_yield = delta_phi_ll_histograms[key].Integral(0, 5)
+        print(key, "yield: ", temp_yield)
+        background_yield += temp_yield
+    print("S/B:", signal_yield/background_yield)
+    print("S/sqrt(S+B):", signal_yield/np.sqrt(signal_yield + background_yield))
 
     ### PLOTTING ###
     # Create a legend
@@ -86,9 +104,12 @@ def main():
 
     colours = [ROOT.kRed, ROOT.kBlack, ROOT.kBlue, ROOT.kGreen]
     maximum = -999
+    stacked_hist = ROOT.THStack()
     for i, key in enumerate(delta_phi_ll_histograms):
       hist = delta_phi_ll_histograms[key]
       hist.SetLineColor(colours[i])
+
+      stacked_hist.Add(hist.Clone())
 
       # Normalise the histograms
       hist.Scale(1./hist.Integral())
@@ -112,6 +133,14 @@ def main():
     # Save the canvas
     canvas.SaveAs("signedDeltaPhill/" + cut + ".pdf")
     canvas.Clear()
+
+    stacked_hist.Draw("hist")
+    leg.Draw('SAME')
+    stacked_hist.GetXaxis().SetTitle(delta_phi_ll_histograms["llll"].GetXaxis().GetTitle())
+    stacked_hist.GetYaxis().SetTitle(delta_phi_ll_histograms["llll"].GetYaxis().GetTitle())
+    canvas.SaveAs('signedDeltaPhill/stack_' + cut + '.pdf')
+    canvas.Clear()
+
     ### END OF PLOTTING ###
 
 if __name__ == "__main__":
